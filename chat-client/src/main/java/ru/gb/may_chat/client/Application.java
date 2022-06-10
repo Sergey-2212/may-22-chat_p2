4ce4466260d033d6a239cafba3e58af946a5a38b
Sergey-2212 <svlakhmatov@gmail.com>
2022-06-10 21:16:08 +0500

    With changes

--- chat-client/src/main/java/ru/gb/may_chat/client/Application.java
+++ chat-client/src/main/java/ru/gb/may_chat/client/Application.java
@@ -1,9 +1,17 @@
 package ru.gb.may_chat.client;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.util.Duration;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.util.Date;
 
 public class Application extends javafx.application.Application {
 
@@ -20,5 +28,12 @@ public class Application extends javafx.application.Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("May chat");
         primaryStage.show();
+        //ChatController controller = loader.getController();
+        //Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(120), x-> {
+        //primaryStage.fireEvent(new WindowEvent(primaryStage, WindowEvent.WINDOW_CLOSE_REQUEST));
+        // }));
+        //timeline.setCycleCount(1);
+        //timeline.play();
+
     }
 }
